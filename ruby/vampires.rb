acf28f708416695e8a0a684a2ecfa4432c3d1351
Jose Balcazar <josebalcazar@joses-mbp.wireless.davidson.edu>
2017-02-19 20:17:11 -0500

    Fix error with end-of input and never-ending while loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,6 @@
-puts "How many employees will be processed"
+puts "How many employees will be processed?"
 num_employees=(gets.chomp.to_i)
-i=0
-while (i!= num_employees)
+until (num_employees==0)
   puts "What is your Name?"
   name= gets.chomp
   puts "How old are you?"
@@ -13,27 +12,25 @@ while (i!= num_employees)
   puts "Would you like to enroll in the company’s health insurance? (y/n)"
   health_insurance = gets.chomp
 
-  if (age == 2017-year_born && (garlic_bread='y' || health_insurance=='y'))
+  if (age == 2017-year_born && (garlic_bread=='y' || health_insurance=='y'))
     puts "Probably not a vampire"
-  elsif (age =! 2017-year_born && (health_insurance='n' || garlic_bread='n'))
+  elsif (age != 2017-year_born && (health_insurance=='n' || garlic_bread=='n'))
     puts "Probably a vampire"
-  elsif (age =! 2017-year_born && (health_insurance='n' && garlic_bread='n'))
+  elsif (age != 2017-year_born && (health_insurance=='n' && garlic_bread=='n'))
     puts "Almost certainly a vampire"
-  elsif (name="Dracula" || name = "Tu Fang")
+  elsif (name=="Dracula" || name == "Tu Fang")
     puts "Defintely a vampire"
   else
     puts "Results inconclusive"
   end
   allergies=''
-  while (allergies =!"sunshine" || allergies=!"Done")
-    puts "Do you have any allergies? If not, write done."
+  until (allergies == "sunshine" || allergies == "None")
+    puts "Do you have any allergies? If so, what are they? If not, write None."
     allergies=gets.chomp
     if allergies == "sunshine"
       puts "Probably a vampire"
       end
   end
-  i=i-1
   num_employees=num_employees-1
-end
-
+  end
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
