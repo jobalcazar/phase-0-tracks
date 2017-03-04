593d254e37e8205b8beaf84984d7c917ed4c094d
Jose Balcazar <josebalcazar@Joses-MacBook-Pro.local>
2017-03-03 16:17:34 -0800

    Set up Puppy.rb for future assignment

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -2,6 +2,18 @@ class Puppy
 end
 Puppy.methods
 
+#Characteristics
+#Puppy Breed: Varies
+#Name: Varies
+#Color: Varies
+#Age: 1 year
+
+#Behavior
+#roll
+#sit
+#stay
+#fetch
+
 duchess = Puppy.new
 fido = Puppy.new
 spot = Puppy.new
@@ -11,3 +23,13 @@ duchess == fido
 fido.instance_of?(Array)
 fido.instance_of?(Puppy)
 spot.play_dead
+
+str = String.new
+str.length
+str.empty?
+str += "huh."
+str = "some string"
+
+greeting = String.new("hello")
+greeting.length
+greeting.upcase
