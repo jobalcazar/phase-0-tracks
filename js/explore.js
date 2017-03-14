f71a3514bebff2445013cba3352599c8c8ad9baa
Jose Balcazar <josebalcazar@joses-mbp.wireless.davidson.edu>
2017-03-14 00:28:40 -0400

    Edit reverse function to fix undefined printing, and add if statement testing if condition is true

--- js/explore.js
+++ js/explore.js
@@ -3,10 +3,15 @@
 
 function reverse(word){
   var new_word="";
-  for (var i=word.length; i>=0; i--){
+  for (var i=word.length-1; i>=0; i--){
     new_word = new_word + word[i];
   }
   return new_word;
 }
 
 //---Driver Code---
+console.log(reverse("Hello"));
+var final_word = reverse("Volcanoe")
+if (1==1){
+  console.log(final_word)
+}
